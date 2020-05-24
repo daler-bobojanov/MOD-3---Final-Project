5b84512e74b455d1c58bfa3d02bfd9a75baf3eda
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-05-23 23:19:21 -0400

    modified

--- your-space/src/App.js
+++ your-space/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <h1>App.js</h1>
+      < LandingPage />
     </div>
   );
 }
