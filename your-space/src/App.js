af82a5f6a120c35166d07a9403afc5028174a03f
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-05-25 00:24:07 -0400

    modified

--- your-space/src/App.js
+++ your-space/src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
-
+import fire from './config/Fire';
 import './App.css';
-import LandingPage from './components/LandingPage';
 
-function App() {
-  return (
-    <div className="">
-      <h1>App.js</h1>
-      < LandingPage />
-    </div>
-  );
+import LandingPage from './components/LandingPage';
+import Home from './components/Home';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      displayName: ''
+    }
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      // console.log(user);
+      if (user) {
+        this.setState({ user: { user }, displayName: user.displayName });
+        localStorage.setItem('user', user.uid);
+        console.log(user);
+
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="">
+        {this.state.user ? (<Home userDisplayName={this.state.displayName} />) : (<LandingPage />)}
+      </div>
+    );
+  }
 }
 
 export default App;
+
