db0d961c8909f538b7f914a75fec0a3f5234fd92
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-05-30 20:10:17 -0400

    modified and code clean-up

--- your-space/src/components/Home.jsx
+++ your-space/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import Navigation from './Navigation';
+import QuotesGenerator from './QuotesGenerator';
 import '../styles/Home.css';
 
 
@@ -8,15 +9,23 @@ const Home = (props) => {
     return (
         <div className="">
             <Navigation />
-            <Card border="warning" style={{ width: '80%', margin: '60px auto' }}>
+            <Card border="warning" style={{ width: '85%', margin: '60px auto' }}>
                 <Card.Header style={{ textAlign: 'center' }}><h1 className="user-greeting">Welcome {props.userDisplayName}</h1></Card.Header>
                 <Card.Body>
-                    <Card.Title className="user-greeting">About Us</Card.Title>
-                    <Card.Text className="user-greeting">
-                        
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptas rem non recusandae ut ratione omnis quia esse molestiae itaque, voluptates eius, laboriosam aliquid? Nam odit itaque velit sint eligendi, iure dolores sequi obcaecati, sit placeat mollitia nemo doloribus amet quasi esse delectus minus. Eligendi mollitia est necessitatibus, enim iusto impedit voluptate! Corrupti inventore ipsa, veniam neque esse quaerat ducimus, dolore amet reprehenderit quasi repellendus unde modi in harum tempora vero tempore nihil nesciunt atque consequatur voluptatem enim consectetur reiciendis. Saepe vitae laborum soluta voluptatibus tenetur, repellat dolores ab iste pariatur, minus, minima nihil quas ipsa nulla quibusdam. Consequatur hic, voluptatibus corrupti voluptates perspiciatis harum minima sint ratione est quia recusandae quas eveniet voluptatem totam reiciendis fugiat. Nemo obcaecati blanditiis veniam nesciunt ipsa nisi distinctio laboriosam beatae minus provident, repellat quos quae accusamus enim eveniet dicta totam? Quas sed ab id earum optio modi dolor fugit eveniet omnis, iure repellendus. Rem et nam architecto provident facilis, molestiae consequatur eum reprehenderit! Quos alias maiores quod culpa, iusto qui veritatis, tenetur perferendis iure et, rerum sunt necessitatibus molestias! Excepturi assumenda harum sint maxime hic repudiandae! Esse est veritatis laboriosam dignissimos assumenda blanditiis consectetur, vitae perferendis ea deleniti molestiae facere natus quidem quo corrupti architecto incidunt odit dolor iure asperiores repudiandae omnis adipisci ipsam cumque!
-                        
+                    <Card.Title className="user-greeting">About "YourSpace"</Card.Title>
+                    <Card.Text className="about">
+                        Capture documents and notes quickly and organize them however you want, no matter where you are.
                     </Card.Text>
+                    <Card.Text className="about">
+                        YourSpace remembers it all so you don’t have to.
+                    </Card.Text>
+                    <Card.Text className="about">
+                        Whether it’s to-do’s or your next big idea, YourSpace makes it easy to add and find later.
+                    </Card.Text>
+                    <Card.Text className="about">
+                        Enjoy!
+                    </Card.Text>
+                    <QuotesGenerator />
                 </Card.Body>
             </Card>
         </div>
